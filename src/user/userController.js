d5dbc187f9f5564cb32b3fc0b66f83b826884280
HarishKumarNagarajan <cnharishnagarah@gmail.com>
2020-04-13 00:02:56 +0530

    jwt

--- src/user/userController.js
+++ src/user/userController.js
@@ -7,14 +7,11 @@ user.use(bodyParser.urlencoded({
 }));
 user.use(bodyParser.json());
 
-
-var jwt = require('jsonwebtoken');
-
-var bcrypt = require('bcryptjs');
-
 var randomize = require('randomatic');
 
 const pool = require('../db/postgres');
+
+var jwtToken = require('../auth/jwtToken');
 
 // user.get('/suma',async function (req, res){
 //     for (var i=0;i<2;i++){
@@ -24,41 +21,27 @@ const pool = require('../db/postgres');
 //     }
 // });
 
-user.get('/MarketPlaces', async function (req, res) {
+user.get('/MarketPlaces', jwtToken, async function (req, res) {
     const client = await pool().connect();
     await client.query("SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT string_agg(t.time_slot_range, ',') as time_slot_ranges from time_slot as t where t.time_slot_id IN (select regexp_split_to_table(time_slot_ids, E',') from market_place_all_details as m where m.market_place_id=mpad.market_place_id)) from market_place_all_details as mpad where mpad.active_check=$1;", ['1'], function (err, result) {
         if (err) {
-            console.log('err in login', err);
+            console.log('err in retreaving marketplaces', err);
             return res.status(500).send({
                 msg: 'Internal error / Bad payload'
             })
         } else {
             if (!result.rows[0]) {
                 return res.status(404).send({
-                    auth: false,
-                    token: null,
                     msg: "No Market-Place details found with the active status"
                 })
             } else {
-                console.log("1");
-                var json=[]
-                for(var j=0;j<result.rowCount;j++){
-                    console.log(j,"1");
-                    json.push(
-                    {
-                        MarketPlaceId: result.rows[j].market_place_id,
-                        MarkwePlaceName:result.rows[j].market_palce_name,
-                        MarketPlaceAddress: result.rows[j].market_place_address,
-                        MarketPlaceTimeSlotRange:result.rows[j].time_slot_ranges
-                    })
-                }
-                return res.status(200).send(json);
-                // console.log(result.rowCount);
+                return res.status(200).send({
+                    marketPlaces: result.rows
+                });
             }
         }
     });
     client.release();
-
 });
 
 module.exports = user;
