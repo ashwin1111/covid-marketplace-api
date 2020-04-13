d909debeb6010ffde919539a9ef94d3c5644ea52
HarishKumarNagarajan <cnharishnagarah@gmail.com>
2020-04-13 12:08:25 +0530

    marketplace query update

--- dummy.sql
+++ dummy.sql
@@ -49,9 +49,12 @@ ALTER TABLE market_place_all_details ADD COLUMN market_place_address text;
 
 ALTER Table count_updates ADD COLUMN time_slot_id text REFERENCES time_slot(time_slot_id);
 
+ALTER table count_updates RENAME COLUMN count to count_on_slot;
+
 --Update Table queries:
 
 update customer_cred SET verified = '0' where verified is NULL;
+
 
 --Insert Table queries:
 
@@ -59,25 +62,13 @@ insert into admin_cred values('aid3ijnuw', 'COVID-19' ,'$2a$08$VQOpb70ElK9339vW5
 
 INSERT INTO time_slot VALUES ('mpidvhclme','9 AM to 9:30 AM',now()),('mpidd2g6f8','9:30 AM to 10 AM',now()),('mpidblijzd','10 AM to 10:30 AM',now()),('mpidkv1mfa','10:30 AM to 11 AM',now()),('mpidtbbnsm','11 AM to 11:30 AM',now());
 
-
 INSERT INTO market_place_all_details (market_place_id,market_palce_name,market_place_address,time_slot_ids,customer_max_count,active_check,created_at) values('mpadidjhsdvf','Shoppers Stop','Pollachi complex 1st floor','mpidvhclme,mpidd2g6f8,mpidblijzd,mpidkv1mfa,mpidtbbnsm',30,'1',now()),('mpadidb40mu2','Fossil','Pollachi main road 1st street','mpidvhclme,mpidd2g6f8,mpidblijzd,mpidkv1mfa',10,'1',now());
 
+INSERT INTO count_updates (count_update_id,market_place_id,time_slot_id,count,created_at) values('cuidywhnl2','mpadidjhsdvf','mpidvhclme',0,now()),('cuidpz4s44','mpadidjhsdvf','mpidd2g6f8',0,now()),('cuid7ch7gk','mpadidjhsdvf','mpidblijzd',0,now()),('cuid4bc1jb','mpadidjhsdvf','mpidkv1mfa',0,now()),('cuid57s1d0','mpadidjhsdvf','mpidtbbnsm',0,now()),('cuidqq7pr1','mpadidb40mu2','mpidvhclme',0,now()),('cuid4fespw','mpadidb40mu2','mpidd2g6f8',0,now()),('cuidd45ss3','mpadidb40mu2','mpidblijzd',0,now()),('cuid1qz921','mpadidb40mu2','mpidkv1mfa',0,now());
 --Select / View queries:
 
 --Market-place_details query:
 
-SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT json_build_object('ids',string_agg(t.time_slot_id,','),'time_slot_ranges', string_agg(t.time_slot_range, ',')) as time_slot from time_slot as t where t.time_slot_id IN (select regexp_split_to_table(time_slot_ids, E',') from market_place_all_details as m where m.market_place_id=mpad.market_place_id)) from market_place_all_details as mpad where mpad.active_check='1';
-
---
-SELECT 
-json_agg(json_build_object('ids',t.time_slot_id,'time_slot_ranges', t.time_slot_range))
-from time_slot as t 
-left join (select regexp_split_to_table(m.time_slot_ids, E',') as time_id, m.customer_max_count as max_cus_count from market_place_all_details as m where m.market_place_id='mpadidb40mu2') as st On st.time_id = t.time_slot_id
-left join  ()
-where st.time_id is NOT NULL AND st.max_cus_count <;
-
-
-left join count_updates as cu ON cu.time_slot_id=t.time_slot_id
-where t.time_slot_id IN (select regexp_split_to_table(time_slot_ids, E',') as time_id 
-                        from market_place_all_details as m 
-                        where m.market_place_id='mpadidjhsdvf');
+-- old:SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT json_build_object('ids',string_agg(t.time_slot_id,','),'time_slot_ranges', string_agg(t.time_slot_range, ',')) as time_slot from time_slot as t where t.time_slot_id IN (select regexp_split_to_table(time_slot_ids, E',') from market_place_all_details as m where m.market_place_id=mpad.market_place_id)) from market_place_all_details as mpad where mpad.active_check='1';
+--new and correct:
+SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT json_agg(json_build_object('id',t.time_slot_id,'time_slot_range', t.time_slot_range,'present_count',cu.count_on_slot )) from time_slot as t left join (select distinct regexp_split_to_table(m.time_slot_ids, E',') as time_id, m.market_place_id as market_place_id, m.customer_max_count as max_cus_count from market_place_all_details as m where m.market_place_id=mpad.market_place_id ) as st On st.time_id = t.time_slot_id left join count_updates as cu on cu.market_place_id=st.market_place_id AND cu.time_slot_id=st.time_id AND cu.count_on_slot < st.max_cus_count where st.time_id is NOT NULL AND cu.count_on_slot is NOT NULL) from market_place_all_details as mpad where mpad.active_check='1';
--- src/user/userController.js
+++ src/user/userController.js
@@ -14,16 +14,16 @@ const pool = require('../db/postgres');
 var jwtToken = require('../auth/jwtToken');
 
 // user.get('/suma',async function (req, res){
-//     for (var i=0;i<2;i++){
+//     for (var i=0;i<10;i++){
 //         var id = await randomize('a0', 6);
-//         id = 'mpadid' + id;
+//         id = 'cuid' + id;
 //         console.log(id);
 //     }
 // });
 
 user.get('/MarketPlaces', jwtToken, async function (req, res) {
     const client = await pool().connect();
-    await client.query("SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT json_build_object('ids',string_agg(t.time_slot_id,','),'time_slot_ranges', string_agg(t.time_slot_range, ',')) as time_slot from time_slot as t where t.time_slot_id IN (select regexp_split_to_table(time_slot_ids, E',') from market_place_all_details as m where m.market_place_id=mpad.market_place_id)) from market_place_all_details as mpad where mpad.active_check=$1;", ['1'], function (err, result) {
+    await client.query("SELECT mpad.market_place_id,mpad.market_palce_name,mpad.market_place_address,(SELECT json_agg(json_build_object('id',t.time_slot_id,'time_slot_range', t.time_slot_range,'present_count',cu.count_on_slot )) from time_slot as t left join (select distinct regexp_split_to_table(m.time_slot_ids, E',') as time_id, m.market_place_id as market_place_id, m.customer_max_count as max_cus_count from market_place_all_details as m where m.market_place_id=mpad.market_place_id ) as st On st.time_id = t.time_slot_id left join count_updates as cu on cu.market_place_id=st.market_place_id AND cu.time_slot_id=st.time_id AND cu.count_on_slot < st.max_cus_count where st.time_id is NOT NULL AND cu.count_on_slot is NOT NULL) from market_place_all_details as mpad where mpad.active_check=$1;", ['1'], function (err, result) {
         if (err) {
             console.log('err in retreaving marketplaces', err);
             return res.status(500).send({
